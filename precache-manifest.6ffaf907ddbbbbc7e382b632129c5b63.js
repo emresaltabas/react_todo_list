aacc5d8705ab5477124988b9272984d85060d644
Emre <emresaltabas@gmail.com>
2022-10-12 20:52:58 +0300

    Updates

---
+++ precache-manifest.6ffaf907ddbbbbc7e382b632129c5b63.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "2f490fe63f79a833f97d",
+    "url": "/react_todo_list/static/css/main.f8866144.chunk.css"
+  },
+  {
+    "revision": "2f490fe63f79a833f97d",
+    "url": "/react_todo_list/static/js/main.2f490fe6.chunk.js"
+  },
+  {
+    "revision": "f12bf432d1b1b25fb939",
+    "url": "/react_todo_list/static/js/1.f12bf432.chunk.js"
+  },
+  {
+    "revision": "fbb65b433cf4bc824bca",
+    "url": "/react_todo_list/static/css/2.c7e4a149.chunk.css"
+  },
+  {
+    "revision": "fbb65b433cf4bc824bca",
+    "url": "/react_todo_list/static/js/2.fbb65b43.chunk.js"
+  },
+  {
+    "revision": "37f812b965df74f153a3",
+    "url": "/react_todo_list/static/js/runtime~main.37f812b9.js"
+  },
+  {
+    "revision": "83de2a792dd5c23de1126dd61e677c9f",
+    "url": "/react_todo_list/index.html"
+  }
+];
